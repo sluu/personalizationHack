2c19745694b945b3d4fa8360220fef3f96c0e099
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2012-02-03 14:10:15 +0000

    Added lovely title

--- src/main/scala/com/gu/personalization/dispatcher/dispatcher.scala
+++ src/main/scala/com/gu/personalization/dispatcher/dispatcher.scala
@@ -95,7 +95,7 @@ class Dispatcher extends ScalatraFilter with ScalateSupport with Logging {
     response.setHeader("Cache-Control", "public, max-age=%d" format cacheMaxAge)
     contentType = contentTypeHeader
     //templateEngine.layout("/WEB-INF/scalate/%s.ssp" format file, renderParams)
-    val htmlString = "<html><head><title></title></head><body><div><ul>" +
+    val htmlString = "<html><head><title></title></head><body><h1>IGuardian</h1><div><ul>" +
       renderParams.get("content").get.asInstanceOf[List[com.gu.openplatform.contentapi.model.Content]].map(articleHtml(_)).mkString("") +
       "</div></body></html>"
     htmlString
@@ -127,7 +127,7 @@ class Dispatcher extends ScalatraFilter with ScalateSupport with Logging {
       content.append(value.map(articleHtml(_)).mkString).append(" <br> ")
     }
 
-    val htmlString = "<html><head><title></title></head><body>" + content.toString()+
+    val htmlString = "<html><head><title></title></head><body><h1>IGuardian</h1>" + content.toString()+
       "</body></html>"
     htmlString
   }
